f9bbebce57f47201687a8ad446f6178526ec839a
Juan Rueda <juan.rueda@stickerstoke.com>
2025-12-16 23:33:49 -0500

    feat: configure rehype-external-links to open external links in new tabs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://pepefeliblu.github.io',
     base: '/dev-rueda',
+    markdown: {
+        rehypePlugins: [
+            [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]
+        ]
+    }
 });
