31535281481950eb4a4a35711a9c24beed6553ea
Juan Rueda <juan.rueda@stickerstoke.com>
2025-12-17 00:21:17 -0500

    feat: Configure `/pepefeliblu` as the base path for GitHub Pages deployment by updating all internal links and asset paths.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import rehypeExternalLinks from 'rehype-external-links';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://pepefeliblu.github.io',
+    base: '/pepefeliblu',
     markdown: {
         rehypePlugins: [
             [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]
