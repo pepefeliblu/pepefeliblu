9dee5c770f0f4c3610d8d5376e650988193a2273
Juan Rueda <juan.rueda@stickerstoke.com>
2025-12-23 13:37:17 -0500

    feat: enhanced rss fed to enable cross-posting

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,11 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt({
+    html: true,
+});
 
 export async function GET(context) {
     const posts = await getCollection('blog');
@@ -12,6 +18,9 @@ export async function GET(context) {
             pubDate: post.data.pubDate,
             description: post.data.description,
             link: `/pepefeliblu/blog/${post.slug}/`,
+            content: sanitizeHtml(parser.render(post.body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img', 'figure', 'figcaption'])
+            }),
         })),
     });
 }
